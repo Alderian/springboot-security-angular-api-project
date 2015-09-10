4de7653acc18ad889317f46d1e8978a55df20a78
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2015-09-10 09:40:08 -0300

    Added part 5 Single Sign On with OAuth2

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # springboot-security-angular-api-project
-Demo using Spring Boot, Spring secutiry and angular. Based on https://spring.io/guides/tutorials/spring-security-and-angular-js
+
+Demo using Spring Boot, Spring security and angular. Based on https://spring.io/guides/tutorials/spring-security-and-angular-js
 
 API Gateway implementation
+Single Sign On with OAuth2
 
-Part 4 of tutorial
+Part 4 & 5 of tutorial
--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -3,9 +3,6 @@ angular.module('hello', [ 'ngRoute' ]).config(function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl : 'home.html',
 		controller : 'home'
-	}).when('/login', {
-		templateUrl : 'login.html',
-		controller : 'navigation'
 	}).otherwise('/');
 
 }).controller('navigation',
@@ -16,51 +13,17 @@ function($rootScope, $scope, $http, $location, $route) {
 		return $route.current && route === $route.current.controller;
 	};
 
-	var authenticate = function(callback) {
-
-		$http.get('user').success(function(data) {
-			if (data.name) {
-				$rootScope.authenticated = true;
-			} else {
-				$rootScope.authenticated = false;
-			}
-			callback && callback();
-		}).error(function() {
+	$http.get('user').success(function(data) {
+		if (data.name) {
+			$rootScope.authenticated = true;
+		} else {
 			$rootScope.authenticated = false;
-			callback && callback();
-		});
-
-	}
-
-	authenticate();
+		}
+	}).error(function() {
+		$rootScope.authenticated = false;
+	});
 
 	$scope.credentials = {};
-	$scope.login = function() {
-		$http.post('login', $.param($scope.credentials), {
-			headers : {
-				"content-type" : "application/x-www-form-urlencoded"
-			}
-		}).success(function(data) {
-			authenticate(function() {
-				if ($rootScope.authenticated) {
-					console.log("Login succeeded")
-					$location.path("/");
-					$scope.error = false;
-					$rootScope.authenticated = true;
-				} else {
-					console.log("Login failed with redirect")
-					$location.path("/login");
-					$scope.error = true;
-					$rootScope.authenticated = false;
-				}
-			});
-		}).error(function(data) {
-			console.log("Login failed")
-			$location.path("/login");
-			$scope.error = true;
-			$rootScope.authenticated = false;
-		})
-	};
 
 	$scope.logout = function() {
 		$http.post('logout', {}).success(function() {
